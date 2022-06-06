a84e771c54d89d3330083cfa548c84af5b9ffa4c
Egor <egor.zhdanko1414@bk.ru>
2022-06-07 02:01:14 +0300

    herod

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
 DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1','localhost',
-                 'onlinequiz-master.com']
+                 'onlinequiz-master.herokuapp.com']
 
 
 # Application definition
